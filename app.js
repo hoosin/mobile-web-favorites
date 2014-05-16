d0840b8837431d760b2eca2d1e25ec323c0aef77
hoosin@yeah.net <hoosin@yeah.net>
2014-05-16 17:01:09 +0800

    cdcx

---
+++ app.js
@@ -0,0 +1,19 @@
+function BodyController($scope) {
+	$scope.greeting = {
+		title: 'Hello',
+		name:'hoosin',
+		type:'Just hoosin',
+		link:'https://github.com/hoosin',
+		libname:'angular',
+		pic:'<img src="https://avatars3.githubusercontent.com/u/3365399?s=460" />',
+		github:'hoosin'
+	};
+}
+
+function head($scope) {
+	$scope.greeting = {
+		title: 'Css3 loading效果',
+		cssUrl:'css/loading.css',
+		jsUrl:'lib/angular.min.js'
+	};
+}
